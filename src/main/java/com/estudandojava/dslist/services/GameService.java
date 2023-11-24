7e0f523f07bc2bb7708266875f971c459953a57a
alan oliveira <alan89oliveira@gmail.com>
2023-11-24 17:46:32 -0300

    endpoint created para game

--- src/main/java/com/estudandojava/dslist/services/GameService.java
+++ src/main/java/com/estudandojava/dslist/services/GameService.java
@@ -2,11 +2,12 @@ package com.estudandojava.dslist.services;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.estudandojava.dslist.dto.GameDTO;
 import com.estudandojava.dslist.dto.GameMinDTO;
 import com.estudandojava.dslist.entities.Game;
 import com.estudandojava.dslist.repositories.GameRepository;
@@ -17,14 +18,21 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly=true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
-		//List<GameMinDTO> dto =  result.stream().map(mapper -> new GameMinDTO(mapper)).toList();
-		
+		//List<GameMinDTO> dto =  result.stream().map(mapper -> new GameMinDTO(mapper)).toList();		
 		List<GameMinDTO> dto = result.stream().collect(
 														Collectors.mapping(x -> new GameMinDTO(x), 
 																Collectors.toList())
 				                                         );
+		return dto;
+	}
+	
+	@Transactional(readOnly=true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
 		return dto;
 	}
 }
